8c881f71f4d32e3c1982e28fbe83011228e66695
PARTH-THAPS <157193049+PARTH-THAPS@users.noreply.github.com>
2025-01-17 23:35:18 +0530

    custom hooks

--- Frontend/src/hooks/useGetAllJobs.jsx
+++ Frontend/src/hooks/useGetAllJobs.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
+import { JOB_API_END_POINT } from "../utils/constant";
+
 
 export default function useGetAllJobs() {
   useEffect(() => {
     const fetchAllJobs = async () => {
       try {
-        const res = await axios.get();
+        const res = await axios.get(`${JOB_API_END_POINT}/get`,{ });
       } catch (err) {}
     };
-  });
+  },[]);
 }
