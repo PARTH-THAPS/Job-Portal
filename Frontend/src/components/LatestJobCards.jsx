7c1f8adbe516c0800625ffe63cd4571479f586cc
PARTH-THAPS <157193049+PARTH-THAPS@users.noreply.github.com>
2025-01-19 22:48:53 +0530

    19jan

--- Frontend/src/components/LatestJobCards.jsx
+++ Frontend/src/components/LatestJobCards.jsx
@@ -5,28 +5,23 @@ const LatestJobCards = ({ job }) => {
   return (
     <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer">
       <div>
-        <h1 className="font-medium text-lg">{job?.company?.name}</h1>
-        <p className="text-sm text-gray-500">India</p>
+        <h1 className="font-medium text-lg">{job?.companyId?.name}</h1>
+        <p className="text-sm text-gray-500">{job?.location}</p>
       </div>
 
       <div>
-        <h1 className="font-bold text-lg my-2">Job Title</h1>
-        <p className="text-sm text-gray-600">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-          architecto facere molestias laudantium provident. Totam a quos
-          perspiciatis nisi temporibus id amet asperiores delectus. Accusantium
-          modi saepe quo deleniti incidunt!
-        </p>
+        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
+        <p className="text-sm text-gray-600">{job?.description}</p>
       </div>
       <div className="flex items-center gap-2 mt-4">
         <Badge className="text-blue-700 font-bold" variant="ghost">
-          12 Positions
+          {job?.position}
         </Badge>
         <Badge className="text-[#F83002] font-bold" variant="ghost">
-          Part Time
+          {job?.jobType}
         </Badge>
         <Badge className="text-[#7209b7] font-bold" variant="ghost">
-          24 LPA
+          {job?.salary}
         </Badge>
       </div>
     </div>
