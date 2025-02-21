ee68509d1b5e3e652702876214dbfef76b518dea
PARTH-THAPS <157193049+PARTH-THAPS@users.noreply.github.com>
2025-02-21 22:10:20 +0530

    DATA SAVED

--- Frontend/admin/CompanyCreate.jsx
+++ Frontend/admin/CompanyCreate.jsx
@@ -44,10 +44,10 @@ export const CompanyCreate = () => {
       <Navbar />
       <div className="max-w-4xl mx-auto">
         <div className="my-10">
-          <h1 className="font-bold text-2xl">Your Comapny Name </h1>
+          <h1 className="font-bold text-2xl">Your Company Name </h1>
           <p>
-            What would you like give your company name ? You can change this
-            later
+            What would you like give your company name ?<br /> You can change
+            this later
           </p>
         </div>
 
